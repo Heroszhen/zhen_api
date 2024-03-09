3f3abc4c8136bbe50a60a0755a6a80e9dd14d6fd
zhen <zhen.yang@hotmail.fr>
2024-03-09 15:40:22 +0100

    [develop] authentificator apikey

---
+++ src/Security/ApiKeyAuthenticator.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+
+class ApiKeyAuthenticator extends AbstractAuthenticator
+{
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return null === $request->headers->get('X-AUTH-API-KEY') ? false : true;
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $apiKey = $request->headers->get('X-AUTH-API-KEY');
+        if (null === $apiKey || '' === $apiKey) {
+            throw new CustomUserMessageAuthenticationException('No Api Key provided');
+        }
+
+        $user = $this->userRepository->findOneBy(['apiKey' => $apiKey]);
+        if (!$user instanceof User) {
+            throw new UserNotFoundException('User not found');
+        }
+
+        return new SelfValidatingPassport(new UserBadge($apiKey, function($apiKey) {
+            $user = $this->userRepository->findOneBy(['apiKey' => $apiKey]);
+            if (!$user instanceof User) {
+                throw new UserNotFoundException('User not found');
+            }
+
+            return $user;
+        }));
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        $data = [
+            // you may want to customize or obfuscate the message first
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+
+            // or to translate this message
+            // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+//    public function start(Request $request, AuthenticationException $authException = null): Response
+//    {
+//        /*
+//         * If you would like this class to control what happens when an anonymous user accesses a
+//         * protected page (e.g. redirect to /login), uncomment this method and make this class
+//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
+//         *
+//         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
+//         */
+//    }
+}
