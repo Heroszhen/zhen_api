d1ea79ea8f7de5e39b3a49f0ae930221fcb91cee
zhen <zhen.yang@hotmail.fr>
2024-03-31 15:00:45 +0200

    maj

--- assets/react/users/users.jsx
+++ assets/react/users/users.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './users.scss';
 import { getToken } from '../utilService';
 
