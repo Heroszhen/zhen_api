e67cd87801fac294d4ac6c29e5d847482b48b9ba
zhen <zhen.yang@hotmail.fr>
2024-04-20 19:16:23 +0200

    maj

--- cmd/update.sh
+++ cmd/update.sh
@@ -4,5 +4,7 @@
 rm -rf var vendor
 rm -rf public/build public/bundles
 php composer.phar install
+php bin/console lexik:jwt:generate-keypair
 php bin/console d:s:u -f
 npm run build
+php bin/console cache:clear
